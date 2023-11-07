6c2a2136401e503c45dc777eccbb39cd68fbd00a
itssherazfaisal <111646492+itssherazfaisal@users.noreply.github.com>
2023-11-07 12:42:19 +0500

    Update initialize_db.py

--- initialize_db.py
+++ initialize_db.py
@@ -1,25 +1,26 @@
-from app import db, User
+from app import app,db, User
 from werkzeug.security import generate_password_hash
 import uuid
 # Create the database and tables
-db.create_all()
+with app.app_context():
+    db.create_all()
 
-# Check if the dummy user already exists
-dummy_user = User.query.filter_by(email='test@gmail.com').first()
+    # Check if the dummy user already exists
+    dummy_user = User.query.filter_by(email='test@gmail.com').first()
 
-if not dummy_user:
-    # Create a new User object with the dummy user's data
-    dummy_user = User(
-        public_id=str(uuid.uuid4()),  # Generate a unique public_id
-        name='demo',
-        email='test@gmail.com',
-        password=generate_password_hash('test123')
-    )
+    if not dummy_user:
+        # Create a new User object with the dummy user's data
+        dummy_user = User(
+            public_id=str(uuid.uuid4()),  # Generate a unique public_id
+            name='demo',
+            email='test@gmail.com',
+            password=generate_password_hash('test123')
+        )
 
-    # Add the dummy user to the database
-    db.session.add(dummy_user)
-    db.session.commit()
+        # Add the dummy user to the database
+        db.session.add(dummy_user)
+        db.session.commit()
 
-    print('Dummy user added successfully.')
-else:
-    print('Dummy user already exists in the database.')
+        print('Dummy user added successfully.')
+    else:
+        print('Dummy user already exists in the database.')
